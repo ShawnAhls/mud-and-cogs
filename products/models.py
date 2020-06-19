d7d1027f3cd89943d79d9a9facdbb0682af8eb4c
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-06-19 10:47:49 +0000

    adding the products to models and to the data base.@

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_lenght=100)
+    friendly_name = models.CharField(max_lenght=100, blank=True)
+
+    def __str__(self):
+        return self.name
+
