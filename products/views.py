3b890ae38cecce6b6aba8a2c5cbf255fb7b99284
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-06-19 21:34:02 +0000

    got the product details to show with only one product

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Products
 
 
@@ -11,3 +11,14 @@ def products(request):
     }
 
     return render(request, "products/products.html", context)
+
+
+def product_details(request, product_id):
+
+    product = get_object_or_404(Products, pk=product_id)
+
+    context = {
+         'product': product,
+    }
+
+    return render(request, "products/product_details.html", context)
