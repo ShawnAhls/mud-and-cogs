c82d812bf6ece9a4de2722352a7c5411a6b731f9
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-06-19 13:18:56 +0000

    Added the categories nd products to the database

--- products/models.py
+++ products/models.py
@@ -2,9 +2,23 @@ from django.db import models
 
 
 class Categories(models.Model):
-    name = models.CharField(max_lenght=100)
-    friendly_name = models.CharField(max_lenght=100, blank=True)
+    name = models.CharField(max_length=100)
+    friendly_name = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.name
 
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Products(models.Model):
+    category = models.ForeignKey(Categories,
+                                 null=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, blank=True)
+
+    def __str__(self):
+        return self.name
