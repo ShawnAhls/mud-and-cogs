8f56bdb7f10efc91b7dfdbcb98fffb6bd47da72b
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-06-20 20:53:40 +0000

    added the search function and the search function works

--- products/views.py
+++ products/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from .models import Products
 
 
 def products(request):
 
     products = Products.objects.all()
+    query = None
+
+    if 'q' in request.GET:
+        query = request.GET['q']
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        products = products.filter(queries)
 
     context = {
          'products': products,
+         'search_term': query,
     }
 
     return render(request, "products/products.html", context)
