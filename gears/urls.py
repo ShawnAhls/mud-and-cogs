df1861beea802461edb0179ea28cb9e5acb58791
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-06-20 22:52:31 +0000

    adding gears table to gears pages

--- gears/urls.py
+++ gears/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.gears, name="gears"),
-    path('<product_id>', views.gear_detail, name="gear_detail"),
+    # path('<gear_id>', views.gear_detail, name="gear_detail"),
 ]
--- gears/views.py
+++ gears/views.py
@@ -1,24 +1,32 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Gears
+from django.db.models import Q
 
 
 def gears(request):
 
     gears = Gears.objects.all()
+    query = None
+
+    if 'q' in request.GET:
+        query = request.GET['q']
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        gears = gears.filter(queries)
 
     context = {
         'gears': gears,
+        'search_term': query,
     }
 
     return render(request, "gears/gears.html", context)
 
 
-def gear_detail(request, product_id):
+# def gear_detail(request, gear_id):
 
-    gear = get_object_or_404(gears, pk=product_id)
+#     gear = get_object_or_404(Gears, pk=gear_id)
 
-    context = {
-         'gear': gear,
-    }
+#     context = {
+#          'gear': gear,
+#     }
 
-    return render(request, "gears/gear_detail.html", context)
+#     return render(request, "gears/gear_detail.html", context)
