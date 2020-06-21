0acfe678df6472b011e6a7b477370d9b55d27939
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-06-21 21:38:48 +0000

    got the parts back and displays under parts

--- parts/urls.py
+++ parts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('parts/', views.parts, name="parts")
+    path('', views.parts, name="parts"),
+    path('<part_id>', views.part_detail, name='part_detail'),
 ]
