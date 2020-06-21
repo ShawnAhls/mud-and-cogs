8e450f52375d7d59c70c4432984b5f91ed273ba9
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-06-21 19:12:40 +0000

    Added the lookup query

--- gears/views.py
+++ gears/views.py
@@ -1,5 +1,5 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
-# def tires(request):
-#     return render(request, "tires/tires.html")
+def gears(request):
+    return render(request, "gears/gears.html")
--- parts/views.py
+++ parts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Parts
+from django.db.models import Q
 
-# Create your views here.
+
+def parts(request):
+
+    parts = Parts.object.all()
+
+    if 'q' in request.GET:
+        query = request.GET['Q']
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        parts = parts.filter(queries)
+
+    context = {
+        'parts': parts,
+        'lookup': query,
+    }
+
+    return render(request, "parts/parts.html", context)
+
+
+def part_detail(request, part_id):
+
+    part = get_object_or_404(Parts, pk=part_id)
+
+    context = {
+        'part': part,
+    }
+
+    return render(request, "parts/part_detail.html", context)
