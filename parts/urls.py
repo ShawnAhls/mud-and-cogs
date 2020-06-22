bf7ebc87e150d3ced257c9421d39ef2960400195
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-06-22 20:23:01 +0000

    Got the single display of a part to display

--- parts/urls.py
+++ parts/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.parts, name="parts"),
-    path('<part_id>', views.part_detail, name='part_detail'),
+    path('<part_id>', views.part_detail, name="part_detail"),
 ]
