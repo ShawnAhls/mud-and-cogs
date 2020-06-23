4b64f23f0dccb8253320de6be51d9fb13211ae48
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-06-23 21:43:54 +0000

    almost done with add ot basket, but got an error noreversematch to sort out

--- basket/views.py
+++ basket/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 
 def basket(request):
     return render(request, "basket/basket.html")
+
+
+def add_to_basket(request, item_id):
+
+    quanity = int(request.POST.get('quanity'))
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quanity
+    else:
+        basket[item_id] = quanity
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return render(request, "basket/basket.html")
