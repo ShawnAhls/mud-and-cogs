8b0c5abf7e32b66b39d01d019ae6c6c683c6574e
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-07-09 16:02:52 +0000

    got the total to add up on the navbar and on the basket page

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,7 +1,5 @@
 from django.shortcuts import get_object_or_404
 from parts.models import Parts
-from decimal import Decimal
-from django.conf import settings
 
 
 def contents(request):
@@ -10,25 +8,6 @@ def contents(request):
     part_count = 0
     total = 0
     basket = request.session.get('basket', {})
-
-    if total < settings.FREE_POSTAGE_THRESHOLD:
-        postage = total * Decimal(settings.STANDARD_POSTAGE_PERCENTAGE / 100)
-        free_postage_delta = settings.FREE_POSTAGE_THRESHOLD - total
-    else:
-        postage = 0
-        free_postage_delta = 0
-
-    grand_total = postage + total
-
-    context = {
-        'basket_items': basket_items,
-        'part_count': part_count,
-        'total': total,
-        'postage': postage,
-        'free_postage_delta': free_postage_delta,
-        'free_postage_threshold': settings.FREE_POSTAGE_THRESHOLD,
-        'grand_total': grand_total,
-    }
 
     for item_id, quantity in basket.items():
         part = get_object_or_404(Parts, pk=item_id)
@@ -39,5 +18,11 @@ def contents(request):
             'quantity': quantity,
             'part': part,
         })
+
+    context = {
+        'basket_items': basket_items,
+        'part_count': part_count,
+        'total': total,
+    }
 
     return context
