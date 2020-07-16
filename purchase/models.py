72a246b1af8a3dcd8fdc3d95ae95621347824914
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-07-16 19:52:04 +0000

    added pillow and countries to my requirements.txt file and created the purchase models file with all the information required to make an purchase and to deliver the parcel

--- purchase/models.py
+++ purchase/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models import Sum
+from parts.models import Parts
+import uuid
+from django_countries.fields import CountryField
 
 
 class Purchase(models.Model):
@@ -13,14 +17,37 @@ class Purchase(models.Model):
     town_or_city = models.CharField(max_length=40)
     county = models.CharField(max_length=80)
     postcode = models.CharField(max_length=20)
-    # country = CountryField(blank_label='Country *')
+    country = CountryField(blank_label='Country *')
     date = models.DateTimeField(auto_now_add=True)
     purchase_total = models.DecimalField(max_digits=10, decimal_places=2,
                                          default=0)
     total = models.DecimalField(max_digits=10, decimal_places=2,
                                 default=0)
-    # original_basket = models.TextField(default='')
+    original_basket = models.TextField(default='')
     stripe_pid = models.CharField(max_length=254, default='')
+
+    def refresh_total(self):
+        self.total = self.purchasepart.aggregate(
+            Sum('purchasepart_total'))['purchasepart_total__sum']
+        self.save()
+
+    def _purchase_number_create(self):
+        return uuid.uuid4().hex.upper()
 
     def __str__(self):
         return self.purchase_number
+
+
+class PurchasePart(models.Model):
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    part = models.ForeignKey(Parts, on_delete=models.CASCADE)
+    quantity = models.IntegerField
+    purchasepart_total = models.DecimalField(max_digits=6, decimal_places=2,
+                                             editable=False)
+
+    def save(self, *args, **kwargs):
+        self.purchasepart_total = self.part.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.order.order_number}'
