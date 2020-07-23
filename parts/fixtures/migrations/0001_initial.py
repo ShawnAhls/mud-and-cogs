6542b351bd51aaecf9399c2b51aab12480dc27eb
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-07-23 10:37:04 +0000

    added the image to the models.py file in parts

--- parts/fixtures/migrations/0001_initial.py
+++ parts/fixtures/migrations/0001_initial.py
@@ -26,6 +26,7 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=150)),
                 ('description', models.TextField()),
                 ('price', models.DecimalField(decimal_places=2, max_digits=6)),
+                ('image', models.imageField),
                 ('url_of_image', models.URLField(max_length=1100)),
                 ('category', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='parts.Categories')),
             ],
