26827ea5a600801f0f1c683a98fe1909dcfdb80f
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-08-02 21:28:45 +0000

    trying to load my static files into the bucket

--- parts/views.py
+++ parts/views.py
@@ -14,8 +14,8 @@ def parts(request):
         queries = Q(name__icontains=query) | Q(description__icontains=query)
         parts = parts.filter(queries)
 
-    if 'categories' in request.GET:
-        category = request.GET['categories']
+    if 'category' in request.GET:
+        category = request.GET['category']
         parts = parts.filter(category__name__in=category)
         category = Category.objects.filter(name__in=category)
 
