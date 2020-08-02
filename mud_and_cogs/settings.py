46a6e3fe9d46fc97dfaa6c27340de742cab0dca3
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-08-02 20:03:42 +0000

    Created an AWS account, custom storage.py file

--- mud_and_cogs/settings.py
+++ mud_and_cogs/settings.py
@@ -181,9 +181,9 @@ if 'USE_AWS' in os.environ:
     # Bucket
     AWS_STORAGE_BUCKET_NAME = 'mud-and-cogs'
     AWS_S3_REGION_NAME = 'eu-east-2'
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and Media Files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
