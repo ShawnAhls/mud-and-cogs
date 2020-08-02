c7f4a3a9f90ed96f683b29337af94b874381aace
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-08-02 20:43:02 +0000

    Changed the position of STATICFILES_DIRS a line above STATIC_URL

--- mud_and_cogs/settings.py
+++ mud_and_cogs/settings.py
@@ -166,11 +166,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Stripe
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
