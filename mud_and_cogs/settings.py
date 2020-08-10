c65378660285f26a04eb0242b6cd994b7eb32a21
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-08-10 18:54:55 +0000

    trying to fix internal error as register was deleted

--- mud_and_cogs/settings.py
+++ mud_and_cogs/settings.py
@@ -9,8 +9,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
-
+# SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = ('fv2brvtl6#scts4+2e51w*o@+py=p%ke$@&%#dl5c-&vhk0s0t')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -65,6 +65,7 @@ TEMPLATES = [
             os.path.join(BASE_DIR, 'templates', 'purchase/purchase.html'),
             os.path.join(BASE_DIR, 'templates', 'parts/parts.html'),
             os.path.join(BASE_DIR, 'templates', 'basket/basket.html'),
+            os.path.join(BASE_DIR, 'templates', 'register/register.html'),
             os.path.join(BASE_DIR, 'templates', 'signin/signin.html'),
             os.path.join(BASE_DIR, 'templates', 'allauth'),
         ],
