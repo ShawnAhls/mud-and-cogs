9c7b106f2b0c13cdfa9e5b6b0b5b9a7c23288519
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-08-10 19:02:42 +0000

    changed the secret key

--- mud_and_cogs/settings.py
+++ mud_and_cogs/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
-# SECRET_KEY = ('fv2brvtl6#scts4+2e51w*o@+py=p%ke$@&%#dl5c-&vhk0s0t')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
