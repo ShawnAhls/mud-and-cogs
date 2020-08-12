82181543045e458e9601a6d6770795fe525a3239
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-08-12 14:01:20 +0000

    Adding all the deleted files for the register app

--- signin/views.py
+++ signin/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def register(request):
-    return render(request, "register/register.html")
+def signin(request):
+    return render(request, "signin/signin.html")
