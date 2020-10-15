5bb16e642cbb39cb1ad2f21092cf2ce28f02e665
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-10-15 18:23:18 +0000

    working on the client_details in stripe.js

--- purchase/static/js/stripe_elements.js
+++ purchase/static/js/stripe_elements.js
@@ -48,15 +48,15 @@ form.addEventListener('submit', function(ev) {
         payment_method: {
             card: card,
             client_details: {
-                name:$.(form.full_name.value),
-                phone:$.trim(form.phone_number.value),
-                email:$.trim(form.email.value),
+                name:(form.full_name.value.trim()),
+                phone:(form.phone_number.value.trim()),
+                email:(form.email.value.trim()),
                 address:{
-                    line1:$.trim(form.street_address1.value),
-                    line2:$.trim(form.street_address2.value),
-                    city:$.trim(form.town_or_city.value),
-                    county:$.trim(form.county.value),
-                    country:$.trim(form.country.value),
+                    line1:(form.street_address1.value.trim()),
+                    line2:(form.street_address2.value.trim()),
+                    city:(form.town_or_city.value.trim()),
+                    county:(form.county.value.trim()),
+                    country:(form.country.value.trim()),
                 }
             }           
         },
